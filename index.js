d71eb1ea8075ba19bbc4256d024dcbe0cf47bc0f
shawonmorshed <shawon@workspaceit.com>
2023-01-17 16:17:55 +0600

    mongo url

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const PORT = process.env.PORT || 5000;
 const mongooseOptions = {
   useNewUrlParser: true,
 };
-
-mongoose
-  .connect("mongodb://localhost:27017/Gateway", mongooseOptions)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Gateway Service api listening on port ${PORT}`);
-    });
+// const mongoUrl = "mongodb://localhost:27017/Gateway";
+const mongoUrl =
+  "mongodb+srv://shawonallproject:wsit97480@cluster0.cvgevwq.mongodb.net/Gateway?retryWrites=true&w=majority";
+mongoose.connect(mongoUrl, mongooseOptions).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Gateway Service api listening on port ${PORT}`);
   });
+});
